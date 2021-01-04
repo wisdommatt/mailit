464ee9b2ea7801b1435e5c2e5077cc0dac09c110
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-01-04 12:09:27 +0100

    Added TextDependencies struct to the package

--- text.go
+++ text.go
@@ -1,3 +1,11 @@
 package mailit
 
-// Dependencies is the
+// TextDependencies is the struct that holds dependencies for
+// a raw text email. e.g smtp host, sender/receiver email etc.
+type TextDependencies struct {
+	SenderEmail   string
+	ReceiverEmail string
+	Subject       string
+	MessageBody   string
+	Attachments   []string
+}
