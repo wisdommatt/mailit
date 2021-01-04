5b94c919233d81ef851ac774d13333b613716933
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-01-04 12:02:14 +0100

    Added project go doc.

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package mailit provides a simple interface that depends on
+// gomail-v2 (https://github.com/go-gomail/gomail) to compose emails
+// and to mail them efficiently without writing complex code.
+//
+// More info on Github: https://github.com/wisdommatt/mailit
+package mailit
